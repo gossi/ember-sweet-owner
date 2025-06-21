4c14e4e1c3d07937430180768e9e66b1d1af7542
gossi <gossi@users.noreply.github.com>
2025-06-21 11:03:46 +0200

    more compat try

--- .try.mjs
+++ .try.mjs
@@ -3,18 +3,20 @@ export default scenarios();
 function scenarios() {
   return {
     scenarios: [
-      compatEmberScenario('ember-lts-3.28', '~3.28.12'),
-      compatEmberScenario('ember-lts-4.12', '~4.12.0'),
-      compatEmberScenario('ember-lts-5.12', '~5.12.0'),
-      emberScenario('~6.4.0'),
-      emberScenario('latest'),
-      emberScenario('beta'),
-      emberScenario('alpha')
+      compatEmberScenario('ember-lts-3.28', '^3.28.0'),
+      compatEmberScenario('ember-lts-4.4', '~4.4.0'),
+      compatEmberScenario('ember-lts-4.12', '^4.12.0'),
+      compatEmberScenario('ember-lts-5.4', '~5.4.0'),
+      compatEmberScenario('ember-lts-5.12', '^5.12.0'),
+      compatEmberScenario('ember-lts-6.4', '~6.4.0'),
+      latestEmberScenario('latest'),
+      latestEmberScenario('beta'),
+      latestEmberScenario('alpha')
     ]
   };
 }
 
-function emberScenario(tag) {
+function latestEmberScenario(tag) {
   return {
     name: `ember-${tag}`,
     npm: {
@@ -35,14 +37,46 @@ module.exports = async function (defaults) {
 };`;
 }
 
+function compatBabel() {
+  return `
+const { babelCompatSupport, templateCompatSupport } = require('@embroider/compat/babel');
+
+module.exports = {
+  plugins: [
+    [ 'babel-plugin-ember-template-compilation', {
+        compilerPath: 'ember-source/dist/ember-template-compiler.js',
+        enableLegacyModules: [
+          'ember-cli-htmlbars',
+          'ember-cli-htmlbars-inline-precompile',
+          'htmlbars-inline-precompile',
+        ],
+        transforms: [...templateCompatSupport()],
+    }],
+    ['module:decorator-transforms', {
+        runtime: { import: require.resolve('decorator-transforms/runtime-esm') },
+    }],
+    ...babelCompatSupport(),
+  ],
+
+  generatorOpts: { compact: false },
+};
+`;
+}
+
 function compatEmberScenario(name, emberVersion) {
+  let cliVersion = '^5.12.0';
+
+  if (emberVersion.includes('3.28')) {
+    cliVersion = '^4.12.0';
+  }
+
   return {
     name,
     npm: {
       devDependencies: {
         'ember-source': emberVersion,
         '@embroider/compat': '^4.0.3',
-        'ember-cli': '^5.12.0',
+        'ember-cli': cliVersion,
         'ember-auto-import': '^2.10.0',
         '@ember/optional-features': '^2.2.0'
       }
@@ -52,6 +86,7 @@ function compatEmberScenario(name, emberVersion) {
     },
     files: {
       'ember-cli-build.js': emberCliBuildJS(),
+      'babel.config.cjs': compatBabel(),
       'config/optional-features.json': JSON.stringify({
         'application-template-wrapper': false,
         'default-async-observers': true,
